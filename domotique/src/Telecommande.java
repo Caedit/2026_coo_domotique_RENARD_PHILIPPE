22aabd18400afa458bac77f507289aebc0ce69a6
Timéo Philippe <timeo.philippe9@etu.univ-lorraine.fr>
2026-04-01 17:57:56 +0200

    ajout du squelette de la Telecommande

---
+++ domotique/src/Telecommande.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Telecommande {
+    //Attibuts
+    private ArrayList <Lampe> lampes;
+
+    public Telecommande() {
+        throw new Error (" code non ecrit ");
+    }
+
+    public void ajouterLampe(Lampe lamp){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerLamper(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void desactiverLamper(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerTout(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public String toString() {
+        throw new Error (" code non ecrit ");
+    }
+}
