5c781a861964d9101b333b19a6fddd6ce27ee39e
Camile Renard <camile.renard6@etu-univ.lorraine.fr>
2026-04-28 10:36:41 +0200

    Ajout de la classe AdapterCheminee Modification du Puml (ajout du toString)

---
+++ domotique/src/AdapterCheminee.java
@@ -0,0 +1,25 @@
+/**
+ * permet d'utiliser une cheminee en tant qu'appareil
+ */
+
+public class AdapterCheminee {
+    private Cheminee ch;
+
+    /**
+     * permet d'allumer
+     */
+    public void allumer() {
+
+    }
+
+    /**
+     * permet d'eteindre
+     */
+    public void eteindre() {
+
+    }
+
+    public String toString() {
+        return ch.toString();
+    }
+}
